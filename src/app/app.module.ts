73751e3b802c878a82d2cf774141222650214b2b
Mauro Mottini <mottinimauro@gmail.com>
2018-10-25 10:50:52 -0300

    Filters component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule
