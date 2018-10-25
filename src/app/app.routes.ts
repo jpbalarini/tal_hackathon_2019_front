625f2e29f2471989c9e5b22bce37f73acc27a267
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2018-10-25 13:36:07 -0300

    Add circle event listeners

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SearchComponent } from './components/search/search.component';
+import { SearchMapComponent } from './components/search-map/search-map.component';
 
 // Route Configuration
 const routes: Routes = [
-  { path: '', component: SearchComponent }
+  { path: '', component: SearchComponent },
+  { path: 'map', component: SearchMapComponent }
 ];
 
 @NgModule({
